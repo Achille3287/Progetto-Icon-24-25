0d82d36fc981d08c7dbd5430b80d7e0c7d82ac4b
Achille3287 <134314183+Achille3287@users.noreply.github.com>
2025-02-05 22:23:31 +0100

    bugfix

--- src/data_ingestion/models/predict.py
+++ src/data_ingestion/models/predict.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import joblib
 import os
-from src.utils.logger import setup_logger, log_message
+from utils.logger import setup_logger, log_message
 
 def predict(file_path, model_path, output_path='dataset/predicted/predicted_results.csv'):
     """
